b681b4288a6fad18b49f8fa297d5b2be070fb749
xuwentao <1294087473@qq.com>
2024-05-21 22:29:44 +0800

    feat: 首页背景大字体

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+'use client';
+
 import bgImgLight from '@public/images/main-background.jpg';
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 import styles from './page.module.css';
 
 export default function Home() {
+  const typeTarget = useRef<any>(null);
+
+  useEffect(() => {
+    const typed = new Typed(typeTarget.current, {
+      strings: [
+        `大江东去，浪淘尽，千古风流人物。故垒西边，人道是，三国周郎赤壁。乱石穿空，惊涛拍岸，卷起千堆雪。江山如画，一时多少豪杰。
+
+        遥想公瑾当年，小乔初嫁了，雄姿英发。羽扇纶巾，谈笑间，樯橹灰飞烟灭。故国神游，多情应笑我，早生华发。人生如梦，一尊还酹江月。`,
+      ],
+      typeSpeed: 60,
+      backSpeed: 0,
+      loop: false,
+      loopCount: Infinity,
+      autoInsertCss: false,
+      backDelay: 2000,
+      showCursor: false,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <main className={styles.home}>
       <div className={styles.bg_card}>
@@ -14,6 +41,20 @@ export default function Home() {
           alt="blog-bg"
           priority={true}
         />
+      </div>
+      {/* 暗黑模式 */}
+      {/* <div className={styles.bg_mask} id="bg_mask" /> */}
+
+      <div className={styles.bg_content}>
+        <div className={styles.title}>念奴娇·赤壁怀古</div>
+        <div className={styles.author}>苏轼</div>
+        <div className={styles.description_box}>
+          <div className={styles.description} ref={typeTarget} />
+        </div>
+
+        {/* 箭头 */}
+        {/* <div className={styles.arrow}>
+        </div> */}
       </div>
     </main>
   );
